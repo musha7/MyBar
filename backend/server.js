964851e86c575c1606e137e67112d91dd23ac6de
Michael Shlezinger <michaelshl7@gmail.com>
2021-01-27 13:38:51 +0200

    mojito picture fix

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,5 @@ else {
 app.use(notFound)
 app.use(errorHandle)
 
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
